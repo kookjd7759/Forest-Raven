449db1546e6199db47d933c6c252e3eaaf3cd762
kookjd7759 <kookjd7759@naver.com>
2024-09-05 15:15:14 +0900

    promotion window

--- UI.py
+++ UI.py
@@ -12,7 +12,7 @@ BOARD_SIZE = 480
 
 
 class GameEndWindow(QDialog): 
-    # loser = 0.win-white 1.win-black 2.Draw-staleMate 3.Draw-piece shortage
+    # loser = 0.win-white 1.win-black 2.Draw-staleMate 3.Draw-By Repetition 4.Draw-piece shortage 
     def __init__(self, parent, callback, ret:Literal[0, 1, 2, 3]): 
         super().__init__(parent)
         self.ret = ret
@@ -49,6 +49,81 @@ class GameEndWindow(QDialog):
         self.callback()
 
 
+class PromotionWindow(QLabel):
+    window_size = (CELL_SIZE + 15) * 2
+    window_point = {
+        'LU': (BOARD_SIZE // 2 - window_size // 2, BOARD_SIZE // 2 - window_size // 2),
+        'RD': (BOARD_SIZE // 2 + window_size // 2, BOARD_SIZE // 2 + window_size // 2)
+    }
+
+    def loadImages(self):
+        parent = self.parent()
+        team = 'w' if self.player == 0 else 'b'
+        self.images = {
+            'q': parent.img[team + 'q'],
+            'n': parent.img[team + 'n'],
+            'r': parent.img[team + 'r'],
+            'b': parent.img[team + 'b']
+        }
+    
+    def __init__(self, parent, player: Literal[0, 1], callback):
+        super().__init__(parent)
+        self.player = player
+        self.callback = callback
+        self.loadImages()
+        self.UIinit()
+
+    def UIinit(self):
+        self.resize(BOARD_SIZE, BOARD_SIZE)
+        
+        background_label = QLabel(self)
+        background_label.setStyleSheet('background-color: rgba(0, 0, 0, 60);')
+        background_label.setFixedSize(BOARD_SIZE, BOARD_SIZE)
+
+        
+        window = QLabel(self)
+        window.resize(self.window_size, self.window_size)
+        window.move(self.window_point['LU'][0], self.window_point['LU'][1])
+
+        window_background = QLabel(self)
+        window_background.setStyleSheet('background-color: rgba(255, 255, 255, 230); border-radius: 4px;')
+        window_background.setFixedSize(self.window_size, self.window_size)
+        window_background.move(BOARD_SIZE // 2 - self.window_size // 2, BOARD_SIZE // 2 - self.window_size // 2)
+        window_background.raise_()
+
+        piece_positions = [(0, 0), (1, 0), (0, 1), (1, 1)]
+        pieces = ['q', 'b', 'r', 'n']
+        
+        layout = QGridLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        for pos, piece in zip(piece_positions, pieces):
+            label = QLabel(self)
+            label.setPixmap(QPixmap(self.images[piece]))
+            label.setFixedSize(CELL_SIZE, CELL_SIZE)
+            label.setScaledContents(True)
+            layout.addWidget(label, pos[0], pos[1])
+        window.setLayout(layout)
+        window.raise_()
+    
+    def on(self, now_x, now_y, dest_x, dest_y):
+        self.raise_()
+        self.show()
+
+    def off(self):
+        self.hide()
+
+    # (0)Q (1)R (2)B (3)N
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            mousePos = self.parent().mapFromGlobal(self.mapToGlobal(event.pos()))
+            x, y = mousePos.x(), mousePos.y()
+            if x >= self.window_point['LU'][0] and y >= self.window_point['LU'][1] and x <= self.window_point['RD'][0] and y <= self.window_point['RD'][1]:
+                idx = (0 if x < BOARD_SIZE // 2 else 1) + (0 if y < BOARD_SIZE // 2 else 2)
+                print(idx)
+            else:
+                print('NO')
+
+
 class HighLightSquare(QLabel):
 
     def __init__(self, parent, UIpos: chess.Position):
@@ -58,7 +133,7 @@ class HighLightSquare(QLabel):
         self.setFixedSize(CELL_SIZE, CELL_SIZE)
         self.hide()
 
-    def on(self, isSelectLight):
+    def on(self, isSelectLight: bool):
         self.raise_()
         if isSelectLight:
             self.setStyleSheet('background-color: rgba(210, 210, 0, 170); border: 2px solid rgba(240, 240, 240, 120);')
@@ -199,6 +274,8 @@ class Window(QWidget):
         for UIx in range(8):
             for UIy in range(8):
                 self.highlight[UIy][UIx] = HighLightSquare(self, chess.Position(UIx, UIy))
+                
+        self.promotion_window = PromotionWindow(self, self.chess.player, self.btn_test_function)
 
     def reset(self):
         self.selected = chess.Position(-1, -1)
@@ -317,7 +394,7 @@ class Window(QWidget):
     def play_move(self, dest: chess.Position):
         check = self.chess.move(self.selected, dest) 
         self.delSelect()
-        # (-1) can't move (0) None (1) CheckMate (2) StaleMate (3) Piece Shortage -> TODO
+        # (-1) can't move (0) None (1) CheckMate (2) StaleMate (3) By Repetition (4) Piece Shortage -> TODO
         if check != -1:
             self.move_piece(self.selected, dest)
             if check == 1:
@@ -351,7 +428,7 @@ class Window(QWidget):
 
 ### Button function 
     def btn_test_function(self):
-        print('test Button')
+        print('test function')
 
     def btn_restart_function(self):
         self.reset()
