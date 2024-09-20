7ba929fa833fc16394a98ff786631cfdcb7781c6
kookjd7759 <kookjd7759@naver.com>
2024-09-20 15:58:43 +0900

    Apply gitignore

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-import UI
-from PyQt5.QtWidgets import QApplication
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = UI.Window()
-    window.show()
-    sys.exit(app.exec_())
